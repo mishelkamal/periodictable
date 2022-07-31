9131758413d7f74861a2c1746e253ba701904119
mishelkamal <mishelkzk@gmail.com>
2022-07-31 20:35:14 +0000

    feat:check-value-atomic-number

--- elements.sh
+++ elements.sh
@@ -6,5 +6,25 @@ if [[ -z $1  ]]
 then 
   echo "Please provide an element as an argument."
 else
-  echo "Hello"
+  NUMCHECK='^[0-9]+$'
+  LENGTH=${#1}
+  echo "length $LENGTH"
+  if [[ $1 =~ $NUMCHECK ]]
+  then
+    ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number = CAST ($1 AS INT)")
+  else
+    if [[ $LENGTH<3 ]]
+    then
+      ATOMIC_NUMBER="$($PSQL "SELECT atomic_number FROM elements WHERE symbol ILIKE '$1'")"
+    else
+      ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE name ILIKE '$1'")
+    fi
+  fi
+  if [[ -z $ATOMIC_NUMBER ]]
+  then
+    echo "Not a valid element"
+  else 
+    echo $ATOMIC_NUMBER
+  fi
+
 fi
