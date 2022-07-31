ff4564385db5c1147fb2947a4ae301a6fa22a7f9
mishelkamal <mishelkzk@gmail.com>
2022-07-31 21:23:29 +0000

    feat:check-value-integrate-database

--- elements.sh
+++ elements.sh
@@ -22,9 +22,17 @@ else
   fi
   if [[ -z $ATOMIC_NUMBER ]]
   then
-    echo "Not a valid element"
+    echo "I could not find that element in the database."
   else 
-    echo $ATOMIC_NUMBER
+    NAME="$($PSQL "SELECT name FROM elements WHERE atomic_number = $ATOMIC_NUMBER")"
+    SYMBOL="$($PSQL "SELECT symbol FROM elements WHERE atomic_number = $ATOMIC_NUMBER")"
+    ATOMIC_MASS="$($PSQL "SELECT atomic_mass FROM elements FULL JOIN properties ON elements.atomic_number=properties.atomic_number WHERE elements.atomic_number = $ATOMIC_NUMBER")"
+    MELTING_POINT_CELSIUS="$($PSQL "SELECT melting_point_celsius FROM elements FULL JOIN properties ON elements.atomic_number=properties.atomic_number WHERE elements.atomic_number = $ATOMIC_NUMBER")"
+    BOILING_POINT_CELSIUS="$($PSQL "SELECT boiling_point_celsius FROM elements FULL JOIN properties ON elements.atomic_number=properties.atomic_number WHERE elements.atomic_number = $ATOMIC_NUMBER")"
+    TYPE="$($PSQL "SELECT types.type FROM elements FULL JOIN properties ON elements.atomic_number=properties.atomic_number FULL JOIN types ON properties.type_id=types.type_id WHERE elements.atomic_number = $ATOMIC_NUMBER")"
+    echo "$NAME ($SYMBOL) $ATOMIC_MASS $BOILING_POINT_CELSIUS $MELTING_POINT_CELSIUS $TYPE"
   fi
 
 fi
+
+#"The element with atomic number 1 is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
