06a2a6e3fe0a3d13a143b141d3e5fcb0d4b2e7b8
mishelkamal <mishelkzk@gmail.com>
2022-08-01 04:55:09 +0000

    feat:print-required-output

--- elements.sh
+++ elements.sh
@@ -8,10 +8,10 @@ then
 else
   NUMCHECK='^[0-9]+$'
   LENGTH=${#1}
-  echo "length $LENGTH"
+  #echo "length $LENGTH"
   if [[ $1 =~ $NUMCHECK ]]
   then
-    ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number = CAST ($1 AS INT)")
+    ATOMIC_NUMBER=$($PSQL "SELECT TRIM(CAST(atomic_number as TEXT)) FROM elements WHERE atomic_number = CAST ($1 AS INT)")
   else
     if [[ $LENGTH<3 ]]
     then
@@ -24,13 +24,16 @@ else
   then
     echo "I could not find that element in the database."
   else 
-    NAME="$($PSQL "SELECT name FROM elements WHERE atomic_number = $ATOMIC_NUMBER")"
+    NAME="$($PSQL "SELECT TRIM(name) FROM elements WHERE atomic_number = $ATOMIC_NUMBER")"
     SYMBOL="$($PSQL "SELECT symbol FROM elements WHERE atomic_number = $ATOMIC_NUMBER")"
     ATOMIC_MASS="$($PSQL "SELECT atomic_mass FROM elements FULL JOIN properties ON elements.atomic_number=properties.atomic_number WHERE elements.atomic_number = $ATOMIC_NUMBER")"
     MELTING_POINT_CELSIUS="$($PSQL "SELECT melting_point_celsius FROM elements FULL JOIN properties ON elements.atomic_number=properties.atomic_number WHERE elements.atomic_number = $ATOMIC_NUMBER")"
     BOILING_POINT_CELSIUS="$($PSQL "SELECT boiling_point_celsius FROM elements FULL JOIN properties ON elements.atomic_number=properties.atomic_number WHERE elements.atomic_number = $ATOMIC_NUMBER")"
     TYPE="$($PSQL "SELECT types.type FROM elements FULL JOIN properties ON elements.atomic_number=properties.atomic_number FULL JOIN types ON properties.type_id=types.type_id WHERE elements.atomic_number = $ATOMIC_NUMBER")"
-    echo "$NAME ($SYMBOL) $ATOMIC_MASS $BOILING_POINT_CELSIUS $MELTING_POINT_CELSIUS $TYPE"
+    R=$(printf "${SYMBOL}")
+    
+    echo "The element with atomic number" $ATOMIC_NUMBER "is" $NAME "("$R"). It's a "$TYPE", with a mass of "$ATOMIC_MASS" amu. "$NAME "has a melting point of "$MELTING_POINT_CELSIUS "celsius and a boiling point of "$BOILING_POINT_CELSIUS "celsius."
+    
   fi
 
 fi
